0bd2556dc5f68df61044e5ef65dcd337d24fbfc8
Minajolie <fayeamina303@gmail.com>
2024-06-25 22:15:47 -0700

    new commit

---
+++ variable.js
@@ -0,0 +1,21 @@
+
+// Écrire un script qui permet de voir sur une page web les différents types de variables avec des exemples.
+let a = "Bonjour a tous";
+document.write(a )
+let b = 5 ;
+document.write(  b )
+let c = true;
+document.write(c )
+let d = [12,13,"ami",[5,"fatou"]];
+document.write(d )
+let e
+document.write(e)
+
+// Déclarez trois variables x, y et z comme "1", "2" et x+y. Afficher le type et la valeur de chaque variable. Ensuite, forcez le type de x et y à entier et exécutez à nouveau a+b. Afficher le type et la valeur de chaque variable.
+let x = 1;
+let y = 2;
+let z = x+y;
+console.log(typeof x + x)
+console.log(typeof y+ y)
+console.log(typeof z + z)
+
